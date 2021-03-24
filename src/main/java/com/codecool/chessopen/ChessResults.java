491e39cfae4fd975a3b0f6ea796db724e6e0564f
Straub Levente <slevy57@gmail.com>
2021-03-24 17:58:35 +0100

    -impo

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -2,7 +2,6 @@ package com.codecool.chessopen;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
